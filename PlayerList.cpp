d72d988f5294c709a60ea926e08b3afad6dd5943
Cody Crofford <codezor4@gmail.com>
2017-03-18 22:35:40 -0500

    Files Added
    This project is a class project for my Data Structures class. Baseball
    Player Data Structures Class. The Main file reads in a txt file with the
    Player class and the PlayerList class sorts it into a ordered linked
    list.

---
+++ PlayerList.cpp
@@ -0,0 +1,211 @@
+/*Player List Class header File
+//Supported Operations:
+1. Default Contructor
+2. Destructor
+3. Add a Player to the List
+4. Iterate the List (enables list traversing)
+5. Clear out the List to make empty
+6. Test if the List is empty
+7. Get the Size of the List
+8. Find a player in the List (get)
+*/
+#include <string>
+#include "PlayerList.h" 
+//#include "Player.h"
+using namespace std;
+
+
+//Node for PlayerList Building 
+
+	Node::Node(const Player &playerIn)
+	{
+		item = playerIn;
+		pNext = NULL;
+	}
+
+
+//Declare PlayerList
+
+	//Constructor 
+	PlayerList::PlayerList()
+	{
+		m_length = 0;
+		pFirst = NULL;
+		pLast = NULL;
+		pCurrent = NULL;
+	}
+
+	//Destructor
+	PlayerList::~PlayerList()
+	{
+		clearList();
+	}
+
+	//Operators 
+
+	/// isEmpty(): bool Retuns true if the list is currently empty
+	bool PlayerList::isEmpty()
+	{
+		if (pFirst == NULL)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	/// add() : void  adds a new player in to the linked list while sorted
+	bool PlayerList::add(Player &playerIn)
+	{
+		Node *pPreviousLocation;			//trailing pointer loacation
+		Node *pNewPlayer;					//new player node
+		pCurrent = pFirst;					// Set current location pointer to first location
+		pPreviousLocation = NULL;			// Set previous location to NULL
+		pNewPlayer = new Node(playerIn);	// assign information to the newPlayer
+											// isEmpty();
+		if (isEmpty()) // then the allocate did not succeed which would be a critical  memory failure!
+		{
+			pNewPlayer->pNext = NULL;   //set the pointer from the new node to the current first address 
+			pFirst = pNewPlayer;
+			m_length++;
+			return true;  // did not succeed
+		}
+
+		while ((pCurrent != NULL) && (pCurrent->item.lessThan(playerIn)))
+		{
+			pPreviousLocation = pCurrent;		//move 
+			pCurrent = pCurrent->pNext;
+		}
+				
+		if (pPreviousLocation == NULL)
+		{
+			pNewPlayer->pNext = pFirst;		//set the pointer from the new node to the current first address 
+			pFirst = pNewPlayer;			//set the new first node to be the begining of the new item
+		}
+
+		else
+		{
+			pNewPlayer->pNext = pCurrent;
+			pPreviousLocation->pNext = pNewPlayer;
+			
+		}
+
+		m_length++;
+		return true;
+	}
+
+	//  removes player from list (perhaps return a statemet if the player was not found or if the player is sucessfully removed
+	void PlayerList::remove(Player &newPlayer)
+	{
+		Node *pPreviousLocation;		// trailing pointer loacation
+		pCurrent = pFirst;				// Set current location pointer to first location
+		pPreviousLocation = NULL;		// Set previous location to NULL
+
+
+		while ((pCurrent != NULL) && (!pCurrent->item.Equal(newPlayer)))
+		{
+			pPreviousLocation = pCurrent;
+			pCurrent = pCurrent->pNext;
+		}
+		// can add nested if to not remove if player is not found in the List
+		if (pPreviousLocation == NULL) // First Node in the list
+		{
+			pFirst = pCurrent->pNext;
+			pPreviousLocation->pNext = pCurrent->pNext;
+			delete pCurrent;
+		}
+
+		if (pCurrent->pNext == NULL) //Last Node in the list
+		{
+			pLast = pPreviousLocation;
+			delete pCurrent;
+		}
+
+		else
+		{
+			pPreviousLocation->pNext = pCurrent->pNext;
+			delete pCurrent;
+		}
+
+		m_length--;
+	}
+
+	/// Retuns number of players in the list
+	int PlayerList::getSize()
+	{
+		return m_length;
+	}
+
+	/// moves the the node position back to the begining of the list
+	void PlayerList::moveToStart()
+	{
+		pCurrent = pFirst;
+	}
+
+	/// Modifies location in the list
+	bool  PlayerList::getNext(Player &playerCall)
+	{
+		bool found;
+		if (pCurrent != NULL)
+		{
+			playerCall = pCurrent->item;
+			pCurrent = pCurrent->pNext;
+			found = true;
+		}
+		else
+		{
+			found = false;
+		}
+
+		return found;
+	}
+
+	// findPlayer : finds a player in the list
+	bool PlayerList::findPlayer(Player &findPlayer)
+	{
+		bool found = false;
+		Node *pPreviousLocation;		// trailing pointer loacation
+		pCurrent = pFirst;				// Set current location pointer to first location
+		pPreviousLocation = pCurrent;	// Set previous location to NULL
+
+		while ((pCurrent != NULL) && (!pCurrent->item.Equal(findPlayer)))
+		{
+			pPreviousLocation = pCurrent;
+			pCurrent = pCurrent->pNext;			
+		}
+		
+		if ((pCurrent != NULL) && (pCurrent->item.Equal(findPlayer)))
+		{			
+			found = true;
+		}
+		
+		return found;
+	}
+
+	//Clear the list
+	void PlayerList::clearList()
+	{
+		Node *pPreviousLocation;	// trailing pointer loacation
+		pCurrent = pFirst;			// Set current location pointer to first location
+		pPreviousLocation = NULL;	// Set previous location to NULL
+
+		while (pCurrent != NULL)
+		{
+			pPreviousLocation = pCurrent;
+			pCurrent = pCurrent->pNext;
+			delete pPreviousLocation;
+		}
+		
+		if (pCurrent != NULL)
+		{
+			delete pCurrent;
+		}
+
+	}
+	void PlayerList::printpcurrent(ostream &outfile)
+	{
+		pCurrent->item.WriteString(outfile);
+	}
+
